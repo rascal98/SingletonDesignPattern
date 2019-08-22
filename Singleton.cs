e4b1498d65143f0b8ecf69d25b991d32a12bb54e
Umut Eskitoğlu <43891600+rascal98@users.noreply.github.com>
2019-08-22 09:42:14 +0300

    Add files via upload

---
+++ Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SingletonOrnek
+{
+    public class SingletonOrnek //Singleton Sınıfımız
+    {
+        private static SingletonOrnek deger = null; //daha önceden oluşmadığını anlamak için null tanımlarız
+
+
+        //Constructor metodumuzu yazıyoruz
+        private SingletonOrnek()
+        {
+
+        }
+        //Dışarıdan ulaşacağımız metod
+        public static SingletonOrnek GetDeger()
+        {
+            return deger ?? (deger = new SingletonOrnek());
+            //Değerimiz boş ise yeni örnek oluşturur döndürür. Değil ise yeni örnek oluşturup döndürür.
+        }
+    }
+}
